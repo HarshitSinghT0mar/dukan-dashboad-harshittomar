91824bf85b59c3a39f495d9bab25ed5473d63103
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2024-01-09 00:07:23 +0530

    add: create OverviewSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import Header from './components/Header'
-import Overview from './components/Overview'
+import OverviewSection from './components/Overview'
 import './global.css'
 
 function App() {
@@ -10,11 +10,8 @@ function App() {
   return (
     <div className='APP_CONTAINER w-full h-screen flex flex-col gap-5 bg-[rgb(250,250,250)]'>
       <Header />
-
-      <div className='flex gap-[20px]'>
-        <Overview heading='Online Orders' number='231'  />
-        <Overview heading='Online Orders' number='₹23,92,312.19' />
-      </div>
+      <OverviewSection />
+     
     </div>
   )
 }
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,6 +1,20 @@
 import * as React from "react";
 
-function Overview({heading, number}) {
+function OverviewSection() {
+  return (
+    <div className="w-full">
+      <p className="text-zinc-900 text-xl font-medium leading-7 mb-[28px]">
+        Overview
+      </p>
+      <div className="flex gap-[20px]">
+      <OverviewCard heading="Online Orders" number="231" />
+      <OverviewCard heading="Online Orders" number="₹23,92,312.19" />
+      </div>
+    </div>
+  );
+}
+
+function OverviewCard({ heading, number }) {
   return (
     <div className="shadow-sm bg-white flex grow flex-col w-full pl-5 pr-20 py-5 rounded-lg items-start max-md:max-w-full max-md:mt-5 max-md:pr-5">
       <div className="text-neutral-600 text-base leading-6 whitespace-nowrap">
@@ -13,5 +27,4 @@ function Overview({heading, number}) {
   );
 }
 
-
-export default Overview
+export default OverviewSection;
